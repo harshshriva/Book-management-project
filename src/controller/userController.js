8f99f07bf6300ad44f29819e9b5ab7dcdced74ab
gktkumartripathi <gtgaurav1005@gmail.com>
2022-05-09 19:01:48 +0530

    user

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,6 +1,6 @@
-const userModels = require("../model/userModel");
-// const jwt = require("jsonwebtoken");
-// const validator = require('validator')
+const userModels = require("../model/userModels");
+ const jwt = require("jsonwebtoken");
+ const validator = require('validator')
 
 const isValid = function(value) {
     if (typeof value === 'undefined' || value === null) return false
@@ -10,6 +10,104 @@ const isValid = function(value) {
 }
 
 // globelly function to validate request body is empty or not
+
+
+
+const createUser = async function (req, res) {
+    try{   
+        let  userBody = req.body
+       
+        if (Object.keys(userBody) == 0) {
+            return res.status(400).send({ status: false, msg: "userDetails must be provided" });
+          }
+        
+    
+       let { title , name, phone , email , password, address} = userBody // destructuring
+    
+     
+       //----------------------------------------------------------------------------------------titleValidation
+       if (!isValid(title))  {
+        res.status(400).send({ status: false, message: 'title is required' })
+           return
+        }
+        
+        //----------------------------------------------------------------------------------------nameValidation
+        if (!isValid(name)) {
+          res.status(400).send({ status: false, message: 'name is required' })
+            return
+        }
+
+        let duplicateName  = await userModels.findOne({name:userBody.name})
+        if(duplicateName){
+            return res.status(400).send({ status:false, msg: ' name already exists'})
+        }
+    
+        //---------------------------------------------------------------------------------------phoneValidation
+        if (!isValid(phone)) {
+            res.status(400).send({ status: false, message: 'phone is required' })
+              return
+        }
+    
+        if (!(/^([+]\d{2})?\d{10}$/.test(userBody.phone))) {
+            return res.status(400).send({ status: false, msg: "please provide a valid phone Number" })
+        }
+    
+        let duplicatePhone  = await userModels.findOne({phone:userBody.phone})
+        if(duplicatePhone){
+            return res.status(400).send({ status:false, msg: 'Phone already exists'})
+        }
+        
+        //---------------------------------------------------------------------------------------emailValidation
+        if (!isValid(email)) {
+            res.status(400).send({ status: false, message: 'Email is required' })
+              return
+        }
+        
+        if (!(/^\w+([\.-]?\w+)@\w+([\. -]?\w+)(\.\w{2,3})+$/.test(userBody.email))) {
+            return res.status(400).send({ status: false, msg: "Please provide a valid email" })
+        }
+    
+        let duplicateEmail  = await userModels.findOne({email:userBody.email})
+        if(duplicateEmail){
+            return res.status(400).send({ status:false, msg: 'email already exists'})
+        }
+    
+        //--------------------------------------------------------------------------------------passwordValidation
+        if (!isValid(password)) {
+            res.status(400).send({ status: false, message: 'password is required' })
+              return
+        }
+    
+        if( !( /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,15}$/.test(userBody.password))) {
+            return res.status(400).send({ status: false, msg: "Please provide a valid password" })
+        }
+
+        let duplicatePassword  = await userModels.findOne({password:userBody.password})
+        if(duplicatePassword){
+            return res.status(400).send({ status:false, msg: 'password already exists'})
+        }
+    
+    //---------------------------------------------------------------------------------------------addressValidation
+     
+    if (!isValid(address)) {
+        res.status(400).send({ status: false, message: 'address is required' })
+          return
+    }
+    
+    
+    
+    
+    
+    //---------------------------------------------------------------------------------------------userCreation
+    const newUser = await userModels.create(userBody)
+    res.status(201).send({ status:true, data:newUser, msg: "user created successfully"})
+    
+    }catch(err){
+        res.status(500).send({status : false , msg : err.message})
+    }
+    }
+    
+
 
 const isValidRequestBody = function(requestBody) {
     return Object.keys(requestBody).length > 0
@@ -65,7 +163,7 @@ const doLogin = async function(req, res) {
 };
 
 
-// module.exports.createuser = createuser;
+module.exports.createUser = createUser;
 module.exports.doLogin = doLogin;
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,9 +2,18 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userController')
 
+
+router.post('/createUser',userController.createUser)
 router.post('/login', userController.doLogin)
 
 
+
+
+
+
+
+
+module.exports = router 
 
 
 
